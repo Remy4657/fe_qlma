1ba89c26d6d9b1f8687c8de34a3663c345d73fac
unknown <trongdatga@gmail.com>
2025-01-10 17:51:14 +0700

    create http

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
+import NextBundleAnalyzer from "@next/bundle-analyzer";
+import createNextIntlPlugin from "next-intl/plugin";
 import type { NextConfig } from "next";
 
+const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "localhost",
+        pathname: "/**",
+      },
+      {
+        hostname: "api-bigboy.duthanhduoc.com",
+        pathname: "/**",
+      },
+      {
+        hostname: "via.placeholder.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
-
-export default nextConfig;
+const withBundleAnalyzer = NextBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+export default withBundleAnalyzer(withNextIntl(nextConfig));
